ec4e77923273dc8d4a5200db9cafeb56f973d31a
b00stfr3ak <goduke44@gmail.com>
2013-08-29 13:24:50 -0700

    SERVER WORKS!
    Added a client array so now we get connections and talk to the
    connections.

--- test-multi-thread/server.rb
+++ test-multi-thread/server.rb
@@ -4,16 +4,17 @@ require './core'
 include MainCommands
 def main_shell()
 	main = Readline.readline('main> ', true)
+	main_shell if main.strip == ''
 	case main.split(' ')[0]
 		when 'list_sessions'
-			list_sessions(@clients)
+			list_sessions(@client_hash)
 		when 'help'
 			help()
 		when 'exit'
-			exit
+			Kernel.exit
 	  when 'use_session'
 	    session_id = main.split(' ')[1]
-			use_session(@clients[:"#{session_id}"]) 
+			use_session(@client_array[session_id.to_i])
 		else
 			print_error("Command #{main} not found\n") 
 			main_shell()
@@ -25,15 +26,17 @@ end
 def command_client(client)
 	loop {
 		command = [(print ("shell> ")), $stdin.gets.rstrip][1]
-		client.print("#{command}")
 		case command
 		  when 'exit'
+				client.print("#{command}\0")
 				exit
 			when ''
 				command_client(client)
       when 'background'
+				print_info("Backgrounding Session!")
 				background()
 		end
+		client.print("#{command}\0")
 		out_put = ''
 		recv_length = 1024
 		while (tmp = client.recv(recv_length))
@@ -44,15 +47,17 @@ def command_client(client)
 	}
 end
 begin
-	@clients = {}
+	@client_hash = {}
 	client_id = "0"
+	@client_array = []
 	server = TCPServer.open(4444)
   Thread.new {
     loop {
-      Thread.start(server.accept) do |client|  
-		    client_name = client.recv(1024)
+      Thread.start(connected_client = server.accept) do |client|  
+				@client_array << connected_client
+				client_name = client.recv(1024)
 			  print_success("#{client_name}\n")
-			  @clients[:"#{client_id}"] = client_name
+			  @client_hash[:"#{client_id}"] = client_name
 			  client_id = client_id.to_i
 			  client_id += 1
 			  client_id = client_id.to_s
